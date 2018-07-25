39fe34388e0273f5c77da5b204e3477ebab10748
Mark F Guerra <markfguerra@gmail.com>
2018-07-25 16:53:58 -0400

    Paste in the application code from the tutorial

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
-import Converter from './Converter';
-const rootElement = document.getElementById('rootElement');
-var conv = new Converter(rootElement, 1.11745);
-conv.render();
+/*
+  external imports
+*/
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+/*
+  internal imports
+*/
+
+import MyComponent from './components/MyComponent';
+
+/*
+  app render
+*/
+
+ReactDOM.render(
+  <MyComponent someData="Jordan" />,
+  document.querySelectorAll('#rootElement')[0]
+);
